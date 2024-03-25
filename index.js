d5030fd8c233f74f8a9974e6dc3d86bf5931886f
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2024-03-25 01:09:13 -0600

    Crear botones en react

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 //root.render(<MyApp />);
 //const button = React.createElement("button", null, "Hello, world!");
-const button = React.createElement("button", {"data-id": 123}, "Hello, world!");
+const button = React.createElement("button", {"data-id": 123}, "Button 1");
+const button2 = React.createElement("button", {"data-id": 456}, "Button 2");
+const button3 = React.createElement("button", {"data-id": 789}, "Button 3");
 
-root.render(button);
+//const div = React.createElement("div", null, button, button2, button3);
+const div = React.createElement(React.Fragment, null, button, button2, button3);
+
+root.render(div);
+
